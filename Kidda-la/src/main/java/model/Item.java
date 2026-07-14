18033970b47883912caed18f4c66fbd2c8972c4a
wataru <wataru.corder@gmail.com>
2026-07-14 21:50:42 +0900

    Itemクラスを追加

---
+++ Kidda-la/src/main/java/model/Item.java
@@ -0,0 +1,38 @@
+package model;
+
+/**
+ * 商品情報を保持するモデルクラスです。
+ */
+public class Item {
+	// 商品ID
+	private int itemId;
+	// 商品名
+	private String itemName;
+	// 値段
+	private int price;
+
+	public int getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
