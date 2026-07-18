e0ae685ae22d7b3fcc8336eaea0774351894aa26
wataru <wataru.corder@gmail.com>
2026-07-18 10:23:56 +0900

    テスト項目の追加(customerSearchByKana(),customerSearch())

--- Kidda-la/src/main/java/test/CustomerSearchDBAccessTest.java
+++ Kidda-la/src/main/java/test/CustomerSearchDBAccessTest.java
@@ -14,17 +14,14 @@ import model.Customer;
  * 対象 CustomerSearchDBAccess Customer
  */
 public class CustomerSearchDBAccessTest {
+	CustomerSearchDBAccess dao = new CustomerSearchDBAccess();
 	@Test
-	void 電話番号検索でCustomerを組み立てて返せる() throws Exception{
-		CustomerSearchDBAccess dao = new CustomerSearchDBAccess();
-		
-		// 実行
+	void 電話番号検索でCustomerを組み立てて返せる() throws Exception {
 		List<Customer> list = dao.searchCustomerByTel("09012345678");
-		
-		// 検証1:1件取得できているかどうか
-		assertFalse(list.isEmpty(), "該当データが取得できていない");
-		
-		// 検証2: CustomerのgetterとDBの値が一致するか
+
+		// 項番1:1件取得できているかどうか
+		assertEquals(1, list.size());
+
 		Customer c = list.get(0);
 		// 項番2:custId
 		assertEquals(1, c.getCustId());
@@ -34,5 +31,47 @@ public class CustomerSearchDBAccessTest {
 		assertEquals("アオキマユミ", c.getKana());
 		// 項番5:address
 		assertEquals("東京都千代田区神田小川町1-1-1", c.getAddress());
+		// 項番6:tel
+		assertEquals("09012345678", c.getTel());
+	}
+
+	@Test
+	void カナ検索でCustomerを組み立てて返せる() throws Exception{
+		List<Customer> list = dao.searchCustomerByKana("アオキマユミ");
+		
+		// 項番1:1件取得できているかどうか
+		assertEquals(1, list.size());
+		
+		Customer c = list.get(0); 
+		// 項番2:custId
+		assertEquals(1, c.getCustId());
+		// 項番3:custName
+		assertEquals("青木まゆみ", c.getCustName());
+		// 項番4:kana
+		assertEquals("アオキマユミ", c.getKana());
+		// 項番5:address
+		assertEquals("東京都千代田区神田小川町1-1-1", c.getAddress());
+		// 項番6:tel
+		assertEquals("09012345678", c.getTel());
+	}
+	
+	@Test
+	void カナ電話番号検索でCustomerを組み立てて返せる() throws Exception{
+		List<Customer> list = dao.searchCustomer("09012345678","アオキマユミ");
+		
+		// 項番1:1件取得できているかどうか
+		assertEquals(1, list.size());
+		
+		Customer c = list.get(0); 
+		// 項番2:custId
+		assertEquals(1, c.getCustId());
+		// 項番3:custName
+		assertEquals("青木まゆみ", c.getCustName());
+		// 項番4:kana
+		assertEquals("アオキマユミ", c.getKana());
+		// 項番5:address
+		assertEquals("東京都千代田区神田小川町1-1-1", c.getAddress());
+		// 項番6:tel
+		assertEquals("09012345678", c.getTel());
 	}
 }
